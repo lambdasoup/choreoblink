edaa06320e17219234886ab5206ac0a010d4a0bb
Max <docoptix@amsf.de>
2018-01-16 12:46:40 +0100

    split out camera code

---
+++ app/src/main/java/com/lambdasoup/choreoblink/camera.kt
@@ -0,0 +1,103 @@
+package com.lambdasoup.choreoblink
+
+import android.app.Application
+import android.arch.lifecycle.AndroidViewModel
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.Transformations
+import android.content.Context
+import android.hardware.camera2.CameraAccessException
+import android.hardware.camera2.CameraManager
+import android.support.v7.widget.CardView
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.TextView
+import java.util.concurrent.ScheduledThreadPoolExecutor
+
+class CameraViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val repository = CameraRepository(application)
+
+    val device = Transformations.map(repository.devices, List<Device>::firstOrNull)
+
+}
+
+class CameraRepository(context: Context) {
+
+    private val manager = context.getSystemService(Context.CAMERA_SERVICE) as CameraManager
+
+    val devices = MutableLiveData<List<Device>>()
+
+    init {
+        try {
+            val ids = manager.cameraIdList
+            devices.postValue(ids.map { id ->
+                val characteristics = manager.getCameraCharacteristics(id)
+                Device(id, 15, 15)
+            })
+        } catch (e: CameraAccessException) {
+            throw RuntimeException(e)
+        }
+    }
+
+}
+
+data class Device(val id: String, val onDelay: Long, val offDelay: Long)
+
+class CameraView @JvmOverloads constructor(context: Context,
+                                           attrs: AttributeSet? = null,
+                                           defStyleAttr: Int = 0)
+    : CardView(context, attrs, defStyleAttr), Observer<Device?> {
+
+    override fun onChanged(device: Device?) {
+        if (device == null) {
+            delay.text = "no camera device available"
+        } else {
+            delay.text = "set delay: ON - ${device.onDelay}ms; OFF - ${device.onDelay}ms"
+        }
+    }
+
+    private val delay: TextView
+
+    init {
+        LayoutInflater.from(context).inflate(R.layout.card_camera, this)
+        delay = findViewById(R.id.delay)
+    }
+
+}
+
+class Torch {
+
+    private val executor = ScheduledThreadPoolExecutor(1)
+
+    var on = false
+
+    internal fun on() {
+        if (on) {
+            return
+        }
+        on = true
+        executor.execute {
+            //            try {
+//                cameraManager!!.setTorchMode(cameraId!!, true)
+//            } catch (e: CameraAccessException) {
+//                e.printStackTrace()
+//            }
+        }
+    }
+
+    internal fun off() {
+        if (!on) {
+            return
+        }
+        on = false
+//        executor.execute {
+//            try {
+//                cameraManager!!.setTorchMode(cameraId!!, false)
+//            } catch (e: CameraAccessException) {
+//                e.printStackTrace()
+//            }
+//        }
+    }
+
+}
--- app/src/main/java/com/lambdasoup/choreoblink/main.kt
+++ app/src/main/java/com/lambdasoup/choreoblink/main.kt
@@ -1,69 +1,39 @@
 package com.lambdasoup.choreoblink
 
 import android.Manifest
-import android.annotation.SuppressLint
 import android.arch.lifecycle.Lifecycle
 import android.arch.lifecycle.LifecycleObserver
 import android.arch.lifecycle.OnLifecycleEvent
 import android.arch.lifecycle.ViewModelProviders
 import android.content.Context
 import android.content.pm.PackageManager
-import android.hardware.camera2.CameraAccessException
-import android.hardware.camera2.CameraCharacteristics
-import android.hardware.camera2.CameraManager
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import android.view.View
 import android.view.WindowManager
 import android.widget.Switch
-import android.widget.TextView
-import java.util.*
 import java.util.concurrent.ScheduledExecutorService
 import java.util.concurrent.ScheduledThreadPoolExecutor
-import java.util.concurrent.TimeUnit
+
+private val TAG = MainActivity::class.java.name
+private val CHOREO = booleanArrayOf(true, false, true, false, true, false, true, false, true, false)
 
 class MainActivity : AppCompatActivity(), TimeSyncView.Listener {
 
-    private val torch = Torch()
-    private val torchCallback = object : CameraManager.TorchCallback() {
-        override fun onTorchModeChanged(cameraId: String, enabled: Boolean) {
-            Log.d(TAG, "torch enabled: " + enabled)
-            if (enabled) {
-                //                onDelay = System.currentTimeMillis() - start;
-            } else {
-                //                offDelay = System.currentTimeMillis() - start;
-            }
-        }
-    }
     private var executor: ScheduledExecutorService? = null
-    private var cameraId: String? = null
-
-    private val onDelay: Long = 0
-    private val offDelay: Long = 0
 
     private lateinit var timeSyncView: TimeSyncView
-
     private lateinit var timeSyncViewModel: TimeSyncViewModel
-
     private lateinit var timestampProvider: TimestampProvider
 
+    private lateinit var cameraView: CameraView
+    private lateinit var cameraViewModel: CameraViewModel
+
     private var showChoreo = false
-    private var cameraManager: CameraManager? = null
-    private var dlay: TextView? = null
-    private val updateTime = Runnable {
-        runOnUiThread {
-            dlay!!.text = String.format(Locale.ENGLISH, "on_delay: %dms", onDelay)
-//            val real = System.currentTimeMillis() + delta
-//            corr!!.text = formatTime(real)
-//
-//            val corr2 = real + onDelay
-//            choreo((corr2 % 10000 / 1000).toInt())
-        }
-    }
+
     private var toggle: Switch? = null
 
     private fun choreo(t: Int) {
@@ -73,132 +43,68 @@ class MainActivity : AppCompatActivity(), TimeSyncView.Listener {
 
         val on = CHOREO[t]
         Log.d(TAG, "on: " + on.toString() + "   t: " + t)
-        if (on) {
-            torch.on()
-        } else {
-            torch.off()
-        }
+//        if (on) {
+//            torch.on()
+//        } else {
+//            torch.off()
+//        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
 
-        cameraManager = getSystemService(CAMERA_SERVICE) as CameraManager
-
+        // bind time stuff
         timeSyncView = findViewById(R.id.time_sync)
         timeSyncView.listener = this
         timeSyncViewModel = ViewModelProviders.of(this)
                 .get(TimeSyncViewModel::class.java)
         timeSyncViewModel.state.observe(this, timeSyncView)
-
         timestampProvider = TimestampProvider(this, timeSyncViewModel::update)
         lifecycle.addObserver(timestampProvider)
 
-        dlay = findViewById(R.id.dlay)
-        toggle = findViewById(R.id.toggle)
-        toggle!!.setOnCheckedChangeListener { buttonView, isChecked ->
-            if (!isChecked) {
-                torch.off()
-            }
+        // bind camera stuff
+        cameraView = findViewById(R.id.camera)
+        cameraViewModel = ViewModelProviders.of(this)
+                .get(CameraViewModel::class.java)
+        cameraViewModel.device.observe(this, cameraView)
 
-            showChoreo = isChecked
-        }
-        findViewById<View>(R.id.delay).setOnClickListener { measureTorchDelay() }
+//        toggle = findViewById(R.id.toggle)
+//        toggle!!.setOnCheckedChangeListener { buttonView, isChecked ->
+//            if (!isChecked) {
+//                torch.off()
+//            }
+//
+//            showChoreo = isChecked
+//        }
 
-        try {
-            val cameraIdList = cameraManager!!.cameraIdList
-            Log.d(TAG, "list: " + cameraIdList)
-            val characteristics = cameraManager!!.getCameraCharacteristics(cameraIdList[0])
-            val available = characteristics.get(CameraCharacteristics.FLASH_INFO_AVAILABLE)
-            Log.d(TAG, "characteristics: " + characteristics)
-            cameraId = cameraIdList[0]
-        } catch (e: CameraAccessException) {
-            e.printStackTrace()
-        }
 
     }
 
-    private fun measureTorchDelay() {
-        if (torch.on) {
-            torch.off()
-        } else {
-            torch.on()
-        }
-    }
-
-    @SuppressLint("NewApi")
     override fun onResume() {
         super.onResume()
 
         timeSyncViewModel.update()
 
-        cameraManager!!.registerTorchCallback(torchCallback, null)
-
         executor = ScheduledThreadPoolExecutor(1)
-        executor!!.scheduleAtFixedRate(updateTime, 0, 10, TimeUnit.MILLISECONDS)
+//        executor!!.scheduleAtFixedRate(updateTime, 0, 10, TimeUnit.MILLISECONDS)
     }
 
     override fun onPause() {
 
-        cameraManager!!.unregisterTorchCallback(torchCallback)
-
         executor!!.shutdown()
 
-        torch.off()
+//        torch.off()
 
         super.onPause()
     }
 
-    inner class Torch {
 
-        var on = false
-
-        internal fun on() {
-            if (on) {
-                return
-            }
-            on = true
-            executor!!.execute {
-                try {
-                    cameraManager!!.setTorchMode(cameraId!!, true)
-                } catch (e: CameraAccessException) {
-                    e.printStackTrace()
-                }
-            }
-        }
-
-        internal fun off() {
-            if (!on) {
-                return
-            }
-            on = false
-            executor!!.execute {
-                try {
-                    cameraManager!!.setTorchMode(cameraId!!, false)
-                } catch (e: CameraAccessException) {
-                    e.printStackTrace()
-                }
-            }
-        }
-
-    }
 
     override fun requestPermission(permission: String) {
         requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 0)
-    }
-
-    companion object {
-
-        private val TAG = MainActivity::class.java.name
-        private val CHOREO = booleanArrayOf(true, false, true, false, true, false, true, false, true, false)
-
-        private fun formatTime(t: Long): String {
-            val ms = t % 1000
-            val s = t / 1000
-            return String.format(Locale.ENGLISH, "%ds%dms", s, ms)
-        }
     }
 
 }
